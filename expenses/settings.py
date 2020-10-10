d1c3e4cc99d6185572d772fe1b8553f4c56ee935
teja <tejaokay@gmail.com>
2020-10-10 14:26:20 +0530

    hopeee

--- expenses/settings.py
+++ expenses/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS= (
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE='whitenoise.django.GzipManifeststaticFilesStorage'
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL ='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'src')
 
