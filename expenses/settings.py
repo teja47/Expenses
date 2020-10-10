d9cecc82eed4053ba0fa701e3d4527dd232be898
teja <tejaokay@gmail.com>
2020-10-10 14:39:48 +0530

    hopeee

--- expenses/settings.py
+++ expenses/settings.py
@@ -125,14 +125,14 @@ CORS_ORIGIN_ALLOW_ALL =True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS= (
+STATICFILES_DIRS= [
      os.path.join(BASE_DIR, 'static'),
-)
+]
 
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL ='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'src')
