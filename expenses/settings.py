9174c27ae9c1e3df077b5446d05f06415d0117b3
teja <tejaokay@gmail.com>
2020-10-10 15:25:39 +0530

    final hope

--- expenses/settings.py
+++ expenses/settings.py
@@ -125,9 +125,9 @@ CORS_ORIGIN_ALLOW_ALL =True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS= [
-#      os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS= [
+     os.path.join(BASE_DIR, 'build/static'),
+]
 
 
 
